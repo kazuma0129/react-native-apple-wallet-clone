d9958deb765685b1db06d98b8f50b4f7657cbed6
KazumaOhashi <tikuwaaaaaaaa@gmail.com>
2021-02-18 04:52:33 +0900

    Update: can create new credit card info

--- App.js
+++ App.js
@@ -3,68 +3,225 @@ import {
   StyleSheet,
   Text,
   View,
-  TextInput,
   ScrollView,
   SafeAreaView,
-  Dimensions,
   TouchableHighlight,
-  Carousel,
-  Image,
-  Linking,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Modal,
+  TouchableOpacity,
   FlatList,
   Pressable,
 } from 'react-native';
 
+import { CreditCardInput } from 'react-native-credit-card-input';
+import { Snackbar, Icon } from 'react-native-magnus';
+
+import * as SecureStore from 'expo-secure-store';
 import * as Haptics from 'expo-haptics';
+import Clipboard from 'expo-clipboard';
+
+const sleep = (second) => new Promise((resolve) => setTimeout(resolve, second * 1000));
+const snackbarRef = React.createRef();
 
 const cardData = {
   name: 'test',
-  num: '0000111122223333',
+  number: '0000 1111 2222 3333',
   MM: '03',
   YY: '25',
+  cvc: '000',
+  type: 'visa',
 };
 
-const cards = [...Array(10)].map((el, i) => ({ ...cardData, id: i }));
+const invisibleCardData = {
+  name: 'test',
+  number: '**** **** **** ****',
+  MM: '**',
+  YY: '**',
+  cvc: '***',
+};
 
-const Card = ({ id, name, num, MM, YY }) => {
-  const [onPress, setOnPress] = useState(false);
+const defaultCards = [...Array(10)].map((el, i) => ({ ...cardData, id: i }));
+
+const Card = ({ id, name, number, MM, YY, cvc, type }) => {
+  const [visibleCardInfo, setVisibleCardInfo] = useState(false);
 
   return (
     <Pressable
-      style={[styles.container, onPress && styles.containerPressed]}
+      style={[styles.container]}
       pressRetentionOffset
       onLongPress={async () => {
         await Haptics.impactAsync();
         setOnPress(true);
       }}
       onPressOut={async () => {
-        await Haptics.impactAsync();
         setOnPress(false);
       }}
     >
+      <TouchableOpacity
+        onPressOut={() => {
+          setVisibleCardInfo(!visibleCardInfo);
+        }}
+      >
+        <Text style={{ color: '#000', fontSize: 20 }}>{visibleCardInfo ? '🙈' : '🙉'}</Text>
+      </TouchableOpacity>
       <Text>{name}</Text>
-      <Text>{`onPress?: ${onPress}`}</Text>
-      <Text>{num}</Text>
-      <Text>{id}</Text>
-      <Text>{`${MM}/${YY}`}</Text>
+      <Text>{type}</Text>
+      <TouchableOpacity
+        onPressOut={async () => {
+          if (!visibleCardInfo) {
+            return;
+          }
+          const removedWhiteSpaceNumber = number.split(' ').join('');
+          Clipboard.setString(removedWhiteSpaceNumber);
+          await Haptics.impactAsync();
+          await sleep(0.2);
+          await Haptics.impactAsync();
+          if (snackbarRef.current) {
+            snackbarRef.current.show();
+          }
+        }}
+      >
+        <Text
+          style={{
+            ...styles.cardInfoTextMargin,
+            fontSize: 32,
+            fontWeight: '300',
+            letterSpacing: 1.5,
+          }}
+        >
+          {visibleCardInfo ? number : invisibleCardData.number}
+        </Text>
+      </TouchableOpacity>
+      <Text style={{ ...styles.cardInfoTextMargin, fontSize: 18 }}>
+        {visibleCardInfo ? `${MM}/${YY}` : `${invisibleCardData.MM}/${invisibleCardData.YY}`}
+      </Text>
+      <Text style={{ ...styles.cardInfoTextMargin, fontSize: 18 }}>
+        {visibleCardInfo ? cvc : invisibleCardData.cvc}
+      </Text>
     </Pressable>
   );
 };
 
 const App = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [cardInfoValid, setCardInfoValid] = useState(false);
+  const [cards, setCard] = useState(defaultCards);
+  const [inputCardInfo, setInputCardInfo] = useState({});
+
   const renderItem = ({ item }) => (
-    <Card id={item.id} name={item.name} num={item.num} MM={item.MM} YY={item.YY} />
+    <Card
+      id={item.id}
+      name={item.name}
+      number={item.number}
+      MM={item.MM}
+      YY={item.YY}
+      cvc={item.cvc}
+      type={item.type}
+    />
   );
 
   return (
     <SafeAreaView style={styles.wrapper}>
       <ScrollView showsVerticalScrollIndicator={false}>
         <View style={styles.viewWrapper}>
-          <Text style={styles.titleText}>Wallet</Text>
-          <Text style={styles.titleText}>+</Text>
+          <Modal animationType='slide' transparent={true} visible={modalVisible}>
+            <TouchableOpacity
+              style={styles.centeredView}
+              activeOpacity={1}
+              onPressOut={() => setModalVisible(false)}
+            >
+              <TouchableWithoutFeedback>
+                <View style={styles.modalView}>
+                  <CreditCardInput
+                    // cardImageFront={0}
+                    // cardImageBack={0}
+                    requiresName={true}
+                    requiresCVC={true}
+                    placeholders={{
+                      name: 'my card',
+                      number: '1234 5678 1234 5678',
+                      expiry: 'MM/YY',
+                      cvc: 'CVC',
+                    }}
+                    labels={{
+                      name: 'LABEL',
+                      number: 'CARD NUMBER',
+                      expiry: 'EXPIRY',
+                      cvc: 'CVC/CCV',
+                    }}
+                    style={{ height: 100 }}
+                    onChange={(form) => {
+                      if (form.valid) {
+                        Keyboard.dismiss();
+                        setCardInfoValid(form.valid);
+                        const [MM, YY] = form.values.expiry.split('/');
+                        const { name, number, cvc, type } = form.values;
+                        const card = {
+                          id: cards.length,
+                          name,
+                          number,
+                          MM,
+                          YY,
+                          cvc,
+                          type,
+                        };
+                        setInputCardInfo(card);
+                      }
+                    }}
+                  />
+
+                  <TouchableHighlight
+                    style={{
+                      backgroundColor: cardInfoValid ? '#2196F3' : '#ff8888',
+                      marginTop: 10,
+                      padding: 10,
+                    }}
+                    disabled={!cardInfoValid}
+                    onPress={() => {
+                      setCard([...cards, inputCardInfo]);
+                      setModalVisible(!modalVisible);
+                    }}
+                  >
+                    <Text style={{ ...styles.textStyle }}>register</Text>
+                  </TouchableHighlight>
+                </View>
+              </TouchableWithoutFeedback>
+            </TouchableOpacity>
+          </Modal>
+          <Text style={styles.titleText}>💳</Text>
+          <TouchableOpacity>
+            <Text
+              style={{ ...styles.titleText }}
+              onPress={() => {
+                setCardInfoValid(false);
+                setModalVisible(true);
+              }}
+            >
+              +
+            </Text>
+          </TouchableOpacity>
         </View>
-        <FlatList data={cards} renderItem={renderItem} keyExtractor={({ id }) => id}></FlatList>
+
+        <FlatList
+          data={cards}
+          inverted={true}
+          renderItem={renderItem}
+          keyExtractor={({ id }) => id}
+        ></FlatList>
       </ScrollView>
+      <Snackbar
+        suffix={<Icon name='checkcircle' color='white' fontSize='md' fontFamily='AntDesign' />}
+        m={40}
+        p={20}
+        onDismiss={() => {}}
+        ref={snackbarRef}
+        bg='gray400'
+        color='black'
+        duration={2000}
+      >
+        Copied to your Clipboard!
+      </Snackbar>
     </SafeAreaView>
   );
 };
@@ -72,6 +229,8 @@ const App = () => {
 const styles = StyleSheet.create({
   wrapper: {
     backgroundColor: '#000',
+    padding: 20,
+    height: '100%',
   },
   viewWrapper: {
     display: 'flex',
@@ -79,10 +238,13 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     margin: 20,
   },
+  cardInfoTextMargin: {
+    marginBottom: 10,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'flex-start',
     justifyContent: 'center',
     height: 220,
     borderColor: '#fff',
@@ -108,6 +270,27 @@ const styles = StyleSheet.create({
     fontSize: 50,
     fontWeight: 'bold',
     color: '#fff',
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    width: 300,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
 });
 
